2e0003fa2f808d280de42244b21edb3c05d4e654
MircoMonti <mircomonti10@gmail.com>
2023-07-19 22:43:33 +0100

    added routing

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+
+//copiare routing da andrea nel progettino di test-react
 
 function App() {
   return (
     <div className="App">
+      ciao
     </div>
   );
 }
